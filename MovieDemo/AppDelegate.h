43c12550cca8118670d7dd980f8da804e60a296c
Ida <ida_sunshine@foxmail.com>
2017-03-20 09:48:47 +0800

    Initial Commit

---
+++ MovieDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieDemo
+//
+//  Created by 腾实信 on 2017/3/20.
+//  Copyright © 2017年 ida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovieDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieDemo
+//
+//  Created by 腾实信 on 2017/3/20.
+//  Copyright © 2017年 ida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
